e36fff63893ec6bab9542a7497f0ae931b786a67
Richard Li <richardli1745@gmail.com>
2024-02-07 20:48:53 -0800

    pass level 1 bonus

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -1,10 +1,16 @@
 import MovieListItem from './MovieListItem.jsx';
 import React from 'react';
-const MovieList = (props) => (
-  <ul>
+const MovieList = (props) => {
+if (props.movies.length !== 0) {
+  return(<ul>
     {props.movies.map((movie,index)=>(
       <MovieListItem key={index} movie={movie}/>
     ))}
-  </ul>
-)
+
+    </ul>)
+} else {
+  return(<div>no movie by that name found</div>)
+}
+}
+
 export default MovieList;
